3c3734bddf537946bca1aebb51c9a7704b694965
Mateus Caetano <mateus.caetano@alu.ufc.br>
2019-11-19 08:18:20 -0300

    update connection with db

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 
 try {
-    mongoose.connect("mongodb://localhost:27017/ecommerce", {
+    mongoose.connect("mongodb+srv://mateus:meusestudos123@ecommerce-1t8wu.mongodb.net/test?retryWrites=true&w=majority", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
