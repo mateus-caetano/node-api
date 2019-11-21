86a7bfb5ac174ecef5d513b52da024dc16872f4a
Mateus Caetano <mateus.caetano@alu.ufc.br>
2019-11-21 09:22:33 -0300

    up

--- src/index.js
+++ src/index.js
@@ -5,21 +5,19 @@ const cors = require('cors')
 
 const app = express()
 
-const conn = () => {
-    try {
-        const c = mongoose.connect("mongodb+srv://mateus:ecommerce@ecommercecluster-innaq.mongodb.net/test?retryWrites=true&w=majority", { 
-            useNewUrlParser: true,
-             useUnifiedTopology: true })
-        console.log(c)
-    } catch(err) {
-        console.log('database not connected')
-    }
+try {
+    const c = mongoose.connect("",
+     { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true 
+    })
+    console.log(c)
+} catch(err) {
+    console.log('database not connected')
 }
 
-conn()
-
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended:true }))
 app.use(cors()) 
 app.use(routes)
 app.listen(3001)
